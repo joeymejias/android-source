bd2ebf3f3a5810092689c36f8dd57ad4f1abcbfe
Joey <joeymejias@gmail.com>
2015-11-15 15:52:49 -0500

    Assignment 17 completed

--- foundation/17-loops/src/com/bloc/loops/Loops.java
+++ foundation/17-loops/src/com/bloc/loops/Loops.java
@@ -11,21 +11,31 @@ public class Loops extends Object {
 		boolean temp = false;
 
 		//********** Starts Here ************************/
-		temp = someBools[7];
-		someBools[7] = someBools[0];
-		someBools[0] = temp;
+		int i = 0;
+		while (i < someBools.length/2) {
+	       temp = someBools[i];
+	       someBools[i] = someBools[someBools.length - i - 1];
+	       someBools[someBools.length - i - 1] = temp;
+	       i++;
+		}
 
-		temp = someBools[6];
-		someBools[6] = someBools[1];
-		someBools[1] = temp;
+		/*
+			temp = someBools[7];
+			someBools[7] = someBools[0];
+			someBools[0] = temp;
 
-		temp = someBools[5];
-		someBools[5] = someBools[2];
-		someBools[2] = temp;
+			temp = someBools[6];
+			someBools[6] = someBools[1];
+			someBools[1] = temp;
 
-		temp = someBools[4];
-		someBools[4] = someBools[3];
-		someBools[3] = temp;
+			temp = someBools[5];
+			someBools[5] = someBools[2];
+			someBools[2] = temp;
+
+			temp = someBools[4];
+			someBools[4] = someBools[3];
+			someBools[3] = temp;
+		*/
 		//********** Ends Here **************************/
 		
 
@@ -50,14 +60,20 @@ public class Loops extends Object {
 		/************************************************/
 
 		//********** Starts Here ************************/
-		numArray[0] = !someBools[0] ? 1 : 0;
-		numArray[1] = !someBools[1] ? 1 : 0;
-		numArray[2] = !someBools[2] ? 1 : 0;
-		numArray[3] = !someBools[3] ? 1 : 0;
-		numArray[4] = !someBools[4] ? 1 : 0;
-		numArray[5] = !someBools[5] ? 1 : 0;
-		numArray[6] = !someBools[6] ? 1 : 0;
-		numArray[7] = !someBools[7] ? 1 : 0;
+	    for (int k = 0; k < 7; k++) {
+	        numArray[k] = !someBools[k] ? 1 : 0;
+	    }
+
+		/*
+			numArray[0] = !someBools[0] ? 1 : 0;
+			numArray[1] = !someBools[1] ? 1 : 0;
+			numArray[2] = !someBools[2] ? 1 : 0;
+			numArray[3] = !someBools[3] ? 1 : 0;
+			numArray[4] = !someBools[4] ? 1 : 0;
+			numArray[5] = !someBools[5] ? 1 : 0;
+			numArray[6] = !someBools[6] ? 1 : 0;
+			numArray[7] = !someBools[7] ? 1 : 0;
+		*/
 		//********** Ends Here **************************/
 
 
